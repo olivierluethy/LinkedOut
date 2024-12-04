439b52a68f5019f4620e9fab2c06fb52711aa0a2
OlivierLuethy <olivier.luethy@gmx.net>
2024-12-04 23:06:08 +0100

    Fix links for download + updated; Add popup fancy background linear gradient

--- pages/downloaded.js
+++ pages/downloaded.js
@@ -2,9 +2,3 @@ document.querySelector(".cta").addEventListener("click", function () {
   window.location.href =
     "https://chromewebstore.google.com/detail/linkedin-feed-cleaner/filpecgdlifajnphchanobkeealliaci/reviews";
 });
-document
-  .querySelector(".header--cta.cta")
-  .addEventListener("click", function () {
-    window.location.href =
-      "https://chromewebstore.google.com/detail/linkedin-feed-cleaner/filpecgdlifajnphchanobkeealliaci/reviews";
-  });
---
+++ pages/update.js
@@ -0,0 +1,13 @@
+document
+  .querySelector(".header--cta.cta")
+  .addEventListener("click", function () {
+    window.location.href =
+      "https://chromewebstore.google.com/detail/x-feed-cleaner/nejdcgfebfamlgpggncmbpkcnefaiemk/reviews";
+  });
+document.getElementById("tryItOut").addEventListener("click", function () {
+  window.location.href = "https://linkedin.com";
+});
+
+document.getElementById("formLink").addEventListener("click", function () {
+  window.location.href = "https://forms.gle/VN6HS4A8UJtefT3u8";
+});
