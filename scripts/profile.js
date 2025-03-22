05f3ac85ef5dc1eba2e58c56e12ef1ad5dcce0cf
OlivierLuethy <olivier.luethy@gmx.net>
2025-03-22 21:08:58 +0100

    Distinguish between your own profile and someone's other

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,11 +1,31 @@
 function profile() {
   console.log("You're inside of profile");
-  const aside = document.querySelector("aside");
-  const childrenToHide = [3, 4, 5, 6];
 
-  childrenToHide.forEach((index) => {
-    if (aside.children[index]) {
-      aside.children[index].style.display = "none";
+  // If exist, your sitting within your own profile
+  const profileSection = document.querySelector(".pv-profile-info-section");
+
+  // Überprüfung, ob das Element existiert
+  if (profileSection) {
+    console.log(
+      "Du befindest dich in deinem eigenen Profil ('.pv-profile-info-section' gefunden)."
+    );
+    const aside = document.querySelector("aside");
+    const childrenToHide = [3, 4, 5, 6];
+
+    childrenToHide.forEach((index) => {
+      if (aside.children[index]) {
+        aside.children[index].style.display = "none";
+      }
+    });
+  } else {
+    console.log(
+      "'.pv-profile-info-section' nicht gefunden, du bist nicht in deinem Profil."
+    );
+    const profilesToFollow = document.querySelector(
+      "aside.scaffold-layout__aside"
+    );
+    if (profilesToFollow) {
+      profilesToFollow.style.display = "none";
     }
-  });
+  }
 }
